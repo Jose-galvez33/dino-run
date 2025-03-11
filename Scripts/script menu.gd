68240bb54b312b9503579be30c451f19022c65a2
Jose-galvez33 <4071020@alu.murciaeduca.es>
2025-03-11 14:30:11 +0100

    Add files via upload

---
+++ Scripts/script menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_jugar_pressed() -> void:
+ get_tree().change_scene_to_file("res://escenas/juego.tscn")
+
+func _on_opciones_pressed() -> void:
+ get_tree().change_scene_to_file("res://escenas/opciones.tscn")
+
+func _on_creditos_pressed() -> void:
+ get_tree().change_scene_to_file("res://escenas/creditos.tscn")
